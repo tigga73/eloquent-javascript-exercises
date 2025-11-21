a0dd51322359443ba9b0144ac3057b966613da78
Tiago Cardoso <tiagocardoso.dev@gmail.com>
2025-11-21 07:26:59 -0300

    first commit

---
+++ chapter4/sumOfRange.js
@@ -0,0 +1,32 @@
+function rangeToArray(start, end, step) {
+  let arr = []
+  if (start < end) {
+    for (let i = start; i <= end; i+= step) {
+      arr.push(i)
+    }
+  } else {
+    for (let i = start; i >= end; i+= step) {
+      arr.push(i);
+    }
+  }
+
+  return arr
+}
+
+function sumOfArray(arr) {
+  if (arr.length === 0) return
+  if (arr.length === 1) return arr[0]
+
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+
+  return sum
+}
+
+console.log(rangeToArray(1, 10))
+console.log(sumOfArray(rangeToArray(1, 10)))
+
+console.log(rangeToArray(1, 10, 2))
+console.log(rangeToArray(5, 2, -1))
