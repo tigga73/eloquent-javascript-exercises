7572fa47a2d5e3ed6cd44d4054e2b2bfd223b78a
Tiago Cardoso <tiagocardoso.dev@gmail.com>
2025-11-22 14:00:06 -0300

    default step and code organization

--- chapter4/sumOfRange.js
+++ chapter4/sumOfRange.js
@@ -1,5 +1,6 @@
-function rangeToArray(start, end, step) {
+function rangeToArray(start, end, step = 1) {
   let arr = []
+
   if (start < end) {
     for (let i = start; i <= end; i+= step) {
       arr.push(i)
@@ -18,6 +19,7 @@ function sumOfArray(arr) {
   if (arr.length === 1) return arr[0]
 
   let sum = 0
+
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i]
   }
